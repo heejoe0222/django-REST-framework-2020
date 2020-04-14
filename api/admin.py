066b629094538207e5552418c490841db9bf7dfd
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2020-04-14 16:13:46 +0900

    add Post, Comment models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from api.models import Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 3
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'likes_count', 'created_at', 'updated_at')
+    inlines = [CommentInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'text', 'likes_count', 'created_at', 'updated_at')
--- api/models.py
+++ api/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
-# 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
+class Post(models.Model):
+    text = models.CharField(max_length=400)
+    likes_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# [제약조건]
-# 1. 1:1과 1:n의 관계 포함
-# 2. 각 모델에 필드 최소 3개 이상 포함
-# 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님)
+    class Meta:
+        managed = True
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, blank=True, null=True, related_name='comment')
+    text = models.CharField(max_length=200)
+    likes_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'

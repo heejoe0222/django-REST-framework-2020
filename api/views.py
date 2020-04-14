d76569403500c89aa03842242b2b4cecaf3d1b5d
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2020-04-14 17:57:03 +0900

    add put and delete function in views.py

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from api.models import Post
 from api.serializers import PostSerializer
 
 class PostList(APIView):
-    def get(self):
+    def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
@@ -27,4 +27,17 @@ class PostDetail(APIView):
         post = self.get_object(pk)
         serializer = PostSerializer(post)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
